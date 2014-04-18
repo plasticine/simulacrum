3e662bb931e6663092100c2d3dc3f9272a801b4f
Justin Morris <justin@pixelbloom.com>
2014-04-18 10:58:55 +1000

    CLI class

--- lib/simulacrum/cli.rb
+++ lib/simulacrum/cli.rb
@@ -1,11 +1,25 @@
+require 'optparse'
 require_relative '../simulacrum'
 require_relative '../simulacrum/command'
+require_relative '../simulacrum/runners/browserstack/runner'
 
 module Simulacrum
   class CLI
     def self.start(*args)
       begin
-        puts args
+        options = {}
+        OptionParser.new do |opts|
+          opts.banner = "Usage: example.rb [options]"
+
+          opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+            options[:verbose] = v
+          end
+        end.parse!
+
+        p options
+        p ARGV
+
+        Simulacrum::Runners::BrowserstackRunner.new(processes: 5)
       rescue => error
         puts error
         exit(1)
