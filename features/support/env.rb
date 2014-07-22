9144eb696943eba30bdc0103ac290b5aa8aab445
Justin Morris <desk@pixelbloom.com>
2014-07-22 12:11:28 +1000

    Lengthen timeout

--- features/support/env.rb
+++ features/support/env.rb
@@ -10,6 +10,6 @@ ENV['PROJECT_ROOT_PATH'] = File.expand_path('../../../', __FILE__)
 Dir.glob('features/step_definitions/**/*steps.rb') { |f| load f, true }
 
 Before do
-  @aruba_timeout_seconds = 60
-  @aruba_io_wait_seconds = 60
+  @aruba_timeout_seconds = 120
+  @aruba_io_wait_seconds = 120
 end
