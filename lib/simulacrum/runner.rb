85c2c8c38cd07aa64e2c759bd208a4b7ec260c84
Justin Morris <desk@pixelbloom.com>
2014-08-17 09:31:29 +1000

    Comment out browserstack usage for now.

--- lib/simulacrum/runner.rb
+++ lib/simulacrum/runner.rb
@@ -1,6 +1,5 @@
 # encoding: UTF-8
 require_relative './runner/base'
-require_relative './runner/browserstack/runner'
 
 module Simulacrum
   # Provide methods for starting the correct runner runner
@@ -10,7 +9,7 @@ module Simulacrum
       when nil
         Simulacrum::Runner::Base.new
       when :browserstack
-        Simulacrum::Runner::BrowserstackRunner.new
+        # Simulacrum::Runner::BrowserstackRunner.new
       end
     end
     module_function :run
